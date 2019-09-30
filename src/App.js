a6aeb924d59412589817e94924f48253728376a9
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-09-29 23:05:43 -0500

    connect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { actions } from './state/actions';
+
+const fetchPath = 'data_melp.json';
 
 class App extends Component {
+
+  componentDidMount = async() => {
+    const { fetchRestaurants } = this.props;
+    try {
+      const response = await fetch(fetchPath);
+      const list = await response.json();
+      fetchRestaurants(list);
+    } catch (error) {
+      console.log(error);
+    }    
+  }
 
   render() {
     return (
@@ -12,5 +27,10 @@ class App extends Component {
   }
 }
 
+const mapDispatch = dispatch => {
+  return {
+    fetchRestaurants: (r) => dispatch(actions.fetchRestaurants(r))
+  }
+};
 
-export default App;
+export default connect(null, mapDispatch)(App);
